1c61b9931948459db3ba5245e9217f16676d75a5
MxDnx <maxime.toussaint.ig@gmail.com>
2020-11-26 14:32:54 +0100

    a
    aZ

---
+++ Forro.mm
@@ -0,0 +1,153 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1606154769642" ID="ID_1365149995" MODIFIED="1606387741729" TEXT="Forro">
+<node CREATED="1606155686900" ID="ID_533922632" MODIFIED="1606155702183" POSITION="left" TEXT="Root">
+<node CREATED="1606156194348" ID="ID_1750906870" LINK="https://youtu.be/ZqQbRqfcidU?t=245" MODIFIED="1606382608654">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Varia&#231;&#227;o da Abertura
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1606157036861" ID="ID_1909874333" LINK="https://youtu.be/9cNYGPfXZ2c?list=PLJYNdFeqDMQp69nvkiv8P0wVFBGFEuOep&amp;t=393" MODIFIED="1606382618688">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Caminhada Invertida
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1606383352403" ID="ID_642978257" MODIFIED="1606383953064" TEXT="Michmich ">
+<node CREATED="1606383570188" ID="ID_283442318" LINK="https://youtu.be/1VGOj7eXPWI?t=156" MODIFIED="1606383729098" TEXT="pull and push"/>
+<node CREATED="1606383592847" ID="ID_858312811" LINK="https://youtu.be/0s5jvyn5PPQ?t=91" MODIFIED="1606383711177" TEXT="menthra "/>
+<node CREATED="1606383954958" ID="ID_1565832583" LINK="https://youtu.be/9fhHQDc7r70?t=23" MODIFIED="1606383980047" TEXT="balayette after sacada"/>
+</node>
+<node CREATED="1606157146760" ID="ID_1517564756" MODIFIED="1606157461795" TEXT="Paulista">
+<node CREATED="1606157156170" ID="ID_1330136468" MODIFIED="1606157160400" TEXT="normal"/>
+<node CREATED="1606157164717" ID="ID_221596420" LINK="https://youtu.be/LUxgKKN8lOc?list=PLJYNdFeqDMQp69nvkiv8P0wVFBGFEuOep&amp;t=192" MODIFIED="1606382633067">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      simple variation
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1606157467040" ID="ID_1816998903" LINK="https://youtu.be/BXkxqH_SHUg?list=PLJYNdFeqDMQp69nvkiv8P0wVFBGFEuOep&amp;t=390" MODIFIED="1606384403361">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      From Piao
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1606157523034" ID="ID_696482805" MODIFIED="1606384445638" TEXT="After Back Hugging caminhada"/>
+<node CREATED="1606384451394" ID="ID_1226034086" MODIFIED="1606384482703" TEXT="After hips holding caminhada"/>
+</node>
+<node CREATED="1606157648579" ID="ID_1820984298" MODIFIED="1606157651072" TEXT="Chap&#xe9;u">
+<node CREATED="1606157653155" ID="ID_520923946" LINK="https://youtu.be/9hYcN3kTBe0?list=PLJYNdFeqDMQp69nvkiv8P0wVFBGFEuOep&amp;t=105" MODIFIED="1606384391676">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      arm high
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1606382669744" ID="ID_1230127882" LINK="https://youtu.be/9hYcN3kTBe0?list=PLJYNdFeqDMQp69nvkiv8P0wVFBGFEuOep&amp;t=194" MODIFIED="1606384376157" TEXT="arm to belly"/>
+</node>
+<node CREATED="1606384794376" ID="ID_1749203482" MODIFIED="1606384845606" TEXT="Sacada">
+<node CREATED="1606384816273" ID="ID_1611858766" MODIFIED="1606384827714" TEXT="normal"/>
+<node CREATED="1606386147945" ID="ID_1452216698" MODIFIED="1606386149627" TEXT="Kick"/>
+</node>
+<node CREATED="1606154891399" ID="ID_791078089" MODIFIED="1606387736609" TEXT="Variation de base">
+<node CREATED="1606154909275" ID="ID_913140374" LINK="https://youtu.be/S1D4JKDkoP8?t=184" MODIFIED="1606382192273">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      basic step avec pas de c&#244;t&#233;
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1606155365275" ID="ID_1165350231" LINK="https://youtu.be/S1D4JKDkoP8?t=261" MODIFIED="1606382266345" TEXT="Basic step avec 2 pas de c&#xf4;t&#xe9;"/>
+<node CREATED="1606155502965" ID="ID_1498223311" LINK="https://youtu.be/S1D4JKDkoP8?t=425" MODIFIED="1606382303523" TEXT="pendulum"/>
+</node>
+<node CREATED="1606155705424" ID="ID_1574690372" MODIFIED="1606155783114" TEXT="Colocar ao lado">
+<node CREATED="1606155771668" ID="ID_197791353" LINK="https://youtu.be/cabbSdhrf1U?t=269" MODIFIED="1606383998531">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Front
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1606155783115" ID="ID_244902475" LINK="https://youtu.be/cabbSdhrf1U?t=310" MODIFIED="1606384008577">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Back
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1606155952624" ID="ID_1642460819" MODIFIED="1606389400205" TEXT="Push and pull">
+<node CREATED="1606155963591" ID="ID_997184291" LINK="https://youtu.be/o0WZOXJttus?list=PLJYNdFeqDMQp69nvkiv8P0wVFBGFEuOep&amp;t=220" MODIFIED="1606384536498">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      End with left foot Check
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1606156115550" ID="ID_427413980" LINK="https://youtu.be/b3342zMCm2I?t=121" MODIFIED="1606382376516">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      normal
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1606156909786" ID="ID_1671146737" LINK="https://youtu.be/o0WZOXJttus?list=PLJYNdFeqDMQp69nvkiv8P0wVFBGFEuOep&amp;t=80" MODIFIED="1606384515016" TEXT="end with blocked caminhada"/>
+</node>
+</node>
+</node>
+</map>
